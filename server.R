31a92e017d021c9a6923a44fe3ad5ba862c0cbab
mpayne61 <mpayne61@users.noreply.github.com>
2017-08-08 10:23:03 +1000

    Data_Products_W4

---
+++ server.R
@@ -0,0 +1,19 @@
+
+# Choose 'USArrests' dataset in the datasets package
+# which is preloaded.
+library(datasets)
+library(dplyr)
+
+# Define a server for the Shiny app
+function(input, output) {
+  
+ output$crimePlot <- renderPlot({
+
+    # Render a barplot
+    barplot(USArrests[,input$crime], 
+            main=input$crime,
+            names.arg = row.names(USArrests),
+            las = 2,
+            ylab="Arrests per 100,000 residents")
+  })
+}
---
+++ ui.R
@@ -0,0 +1,29 @@
+# Rely on the 'WorldPhones' dataset in the datasets
+# package (which generally comes preloaded).
+library(datasets)
+library(dplyr)
+
+# Use a fluid Bootstrap layout
+fluidPage(    
+  
+  # Give the page a title
+  titlePanel("Violent Crime Rates by US State"),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      selectInput("crime", "Crime:", 
+                  choices=colnames(USArrests)),
+      hr(),
+      helpText("Data from World Almanac and Book of facts 1975")
+    ),
+    
+    # Create a barplot place
+    mainPanel(
+      plotOutput("crimePlot")  
+    )
+    
+  )
+)
